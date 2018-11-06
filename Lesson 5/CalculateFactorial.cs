fc7d09fee4728b9a20b7e6d38d86f9bf6d4891ef
Yurrko <yurrko@yandex.ru>
2018-11-06 21:38:42 +0300

    hw

---
+++ Lesson 5/CalculateFactorial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    class CalculateFactorial
+    {
+        private int _n;
+        private double _f;
+        private double _sum;
+        private string _threadName;
+
+        public CalculateFactorial( int n, string threadName)
+        {
+            _n = n;
+            _threadName = threadName;
+            _f = 1;
+            _sum = 0;
+        }
+
+        public void CalculateFactorialInThread()
+        {
+            //Console.WriteLine(_threadName);
+            for ( var i = 1; i <= _n; i++ )
+            {
+                _f *= i;
+            }
+
+            Console.WriteLine($"Factorial = {_f}");
+        }
+
+        public void CalculateSumOfNumbersInThread()
+        {
+            //Console.WriteLine(_threadName);
+            for (int i = 0; i < _n; i++)
+            {
+                _sum += i;
+            }
+            Console.WriteLine( $"Sum = {_sum}" );
+        }
+    }
+}
---
+++ Lesson 5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var N = 0;
+            var F = 1;
+            var enabled = true;
+            while ( enabled )
+            {
+                Console.WriteLine( "Введите число" );
+                enabled = int.TryParse(Console.ReadLine(), out N);
+
+                var calClass = new CalculateFactorial( N, "second thread" );
+                var thr1 = new Thread(calClass.CalculateFactorialInThread);
+                var thr2 = new Thread(calClass.CalculateSumOfNumbersInThread);
+                thr1.Start();
+                thr2.Start();
+            };
+        }
+    }
+}
